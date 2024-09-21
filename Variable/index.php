4401f6d6f7d8c32aa2f5e12558287fd9e2a97a0e
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-21 13:52:27 +0530

    variable create

--- Variable/index.php
+++ Variable/index.php
@@ -5,6 +5,16 @@
     <title>Variable</title>
 </head>
 <body>
-<h1>hi</h1>
+<h1>
+<?php
+    $name = "kamala"; // String
+    $age = 25;        // Integer
+    $isStudent = true; // Boolean
+
+    echo $name;
+    echo $age;  
+    echo $isStudent;
+?>
+</h1>
 </body>
 </html>
