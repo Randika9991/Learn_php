6b8ac271f25a72e7e07cbb63a5446817c3a8dc1d
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-21 17:56:18 +0530

    Conditionals and Booleans

---
+++ Conditionals and Booleans/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Variable</title>
+</head>
+<body>
+<?php
+            // Declaring variables
+            $name = "kamala";
+            $age = 25;
+            $isStudent = true;
+
+        
+        if($isStudent){
+            $massage = "this massage is true.";
+        }else{
+            $massage = "this massage is false.";
+        }
+        ?>
+
+    <h1>
+        <?= $massage ?>
+    </h1>
+</body>
+</html>
