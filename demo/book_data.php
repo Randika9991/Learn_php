d4c92469e4dc1f650d9e13dbd832a866b0052830
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-21 13:06:26 +0530

    first commit

---
+++ demo/book_data.php
@@ -0,0 +1,21 @@
+<?php
+// book_data.php
+
+$book = [
+    [
+        "title" => "The Great Gatsby",
+        "author" => "F. Scott Fitzgerald",
+        "year" => 1925
+    ],
+    [
+        "title" => "The Great Gatsby",
+        "author" => "F. Scott Fitzgerald",
+        "year" => 1925
+    ]
+];
+
+function name() {
+    echo strlen("Hello world!");
+    return 'kamala';
+}
+
