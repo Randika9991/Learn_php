a65b3da7e74cf3d2e6055e07eaabd7a897042e31
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-23 18:31:48 +0530

    Introduction to Authorization

---
+++ Introduction to Authorization/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require "Functions.php";
+
+//dd($_SERVER);
+//dd(parse_url($uri));
+//db
+require 'Database.php';
+
+//router
+require 'router.php';
+
+
+
+
+
+//$posts = $db->query("SELECT * FROM customer")->fetchAll(PDO::FETCH_ASSOC);
+
+//foreach ($posts as $post)
+//{
+//    echo "<li>" . $post['custName'] . "</li>";
+//    echo "<li>" . $post['custId'] . "</li>";
+//}
+
+
