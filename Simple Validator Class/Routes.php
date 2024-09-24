cd3e60a98f1f49c7fdc50ce46a39bb7299d171d6
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-24 12:35:19 +0530

    Simple Validator Class

---
+++ Simple Validator Class/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/About.php',
+    '/contact' => 'controllers/Contact.php',
+    '/book' => 'controllers/book.php',
+    '/notes/create' => 'controllers/note-create.php',
+    '/notes' => 'controllers/Note.php',
+];
---
+++ Simple Validator Class/views/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php require "views/partials/head.php" ?>
+<html lang="en" class="h-full bg-gray-100">
+<body class="h-full">
+<div class="min-h-full">
+
+    <?php require "views/partials/nav.php" ?>
+
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+        </div>
+    </main>
+</div>
+</body>
+</html>
