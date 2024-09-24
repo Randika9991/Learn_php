c89e20ba766abd5838960c2593558b68f22997d1
Randika9991 <kumarasirirandika0706@gmail.com>
2024-09-24 11:47:17 +0530

    Form Validation

---
+++ Form Validation/controllers/Note.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+//$posts = $db->query("SELECT * FROM customer")->fetchAll(PDO::FETCH_ASSOC);
+
+
+$heading = 'Note';
+
+$notes = $db->query('SELECT * FROM notes WHERE id = 1')->get();
+//var_dump($notes);
+
+
+//dd($notes);
+
+require "views/Note.view.php";
